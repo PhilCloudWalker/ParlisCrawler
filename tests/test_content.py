69bc6b1965284344e8a210623301e6bc5e13eed7
Philipp D'Angelo <thinkport@Azats-MacBook-Pro.local>
2020-08-02 11:58:18 +0200

    adjust test

--- tests/test_content.py
+++ tests/test_content.py
@@ -9,18 +9,15 @@ from ParlisCrawler.Content import ContentOverview
 ########################################################
 
 #1) check year function
-@pytest.mark.content
 def test_cv_year():
     test = ContentOverview('title', 'link', '2019-06-02')
     assert ( test.year == 2019)
 
-@pytest.mark.content
 def test_cv_title_input():
     test = ContentOverview('title', 'link', '2019-06-02')
     with pytest.raises(ValueError):
         test.title=2
 
-@pytest.mark.content
 def test_cv_date_input():
     test = ContentOverview('title', 'link', '2019-06-02')
     with pytest.raises(ValueError):
